f7ac37ad44ad25f16914afd862235f748fb7be01
laure leter <laure.leter@gmail.com>
2021-03-13 20:01:19 +0100

    readme

--- OpenFoodFactsCategorizer/data.py
+++ OpenFoodFactsCategorizer/data.py
@@ -7,12 +7,14 @@ def get_data_from_image():
     """If OCR integrated, Expects a path to a folder with images and get ocr text for each of them"""
     pass
 
-def get_data(path):
+def get_data(path='.'):
+    # set the path in parameters
     df = pd.read_csv(path)
 
     return df
 
-def get_data_from_text(n_rows=1000, path='raw_data/ocr_labeled_1K.csv', holdout=0.3):
+def get_data_from_text(path='.', holdout=0.3):
+    # set the path in parameters
     """Expects a path to a csv with ocr text and labels; Returns splitted train-test data"""
     # ADD JSON TO CSV
 
@@ -33,4 +35,4 @@ def get_data_from_text(n_rows=1000, path='raw_data/ocr_labeled_1K.csv', holdout=
 if __name__ == '__main__':
     get_data_from_text()
 
-#Coucou c'est Aoife ! Bisous bisous 
+#Coucou c'est Aoife ! Bisous bisous
