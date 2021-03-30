1d625225ec811e3ea1db24c0297d09b8c1415d55
laure leter <laure.leter@gmail.com>
2021-03-30 10:43:31 +0200

    test predict with api OCR

--- OpenFoodFactsCategorizer/data.py
+++ OpenFoodFactsCategorizer/data.py
@@ -4,17 +4,6 @@ import requests
 
 from sklearn.model_selection import train_test_split
 
-
-# Get data to use our ridge model
-
-def get_data_from_ocr(url):
-    """Expects url to a json ocr -from image- """
-    result = requests.get(url)
-    json = result.json()
-    text = json['responses'][0]['fullTextAnnotation']['text']
-    return text
-
-
 ### Get data to train a new model
 
 def get_data(path='.'):
@@ -39,4 +28,3 @@ def get_data_from_text(path='.', holdout=0.3):
 
 
 #if __name__ == '__main__':
-#print(get_data_from_ocr('https://static.openfoodfacts.org/images/products/00390804/1.json'))
